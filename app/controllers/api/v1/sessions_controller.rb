f9c884537a3e3b699a5dce821d65ea59af6847df
RaviShankarArya <rshankarganeshrao@gmail.com>
2017-08-28 18:53:50 +0530

    added line

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,7 @@ module Api
 		class SessionsController < ApplicationController
 			def create
 				@user = User.find_by_email params[:email]
-				if @user && @user.authenticate(params[:password])
+				if @user && @user.authenticate(params[:password]) 
 					@user.update_attribute(:auth_token , @user.generate_auth_token)
 					render :json => {:login_response => @user.as_json(:only => [:id, :user_name, :email, :phone, :address, :auth_token]).merge({:profile => @user.try(:user_profile).try(:profile_url)})}, :status => 200
 				else
